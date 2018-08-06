7d95c426904ad227724e5a7b7a7a1d6adf10c3e5
Mario Amore <quaaludes@gmail.com>
2018-08-06 09:14:37 +0800

    Add state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    venue: locations,
+    searchedVenues: '',
+    placeToShow: '',
+    isOpen: false,
+    error: false,
+    infoContent: ""
+  }
+
   render() {
     return (
       <div className="App">
