d212baab430827bfeb6434194da0b6ba9d554b62
Mario Amore <quaaludes@gmail.com>
2018-08-08 09:12:25 +0800

    Change the component into a pure one

--- src/MarkerInfoWindow.js
+++ src/MarkerInfoWindow.js
@@ -4,34 +4,30 @@ import { Marker, InfoWindow } from "react-google-maps"
 require("recompose");
 
 
+const MarkerInfoWindow = (props) => {
 const iconDefault = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
 const iconOpen = 'http://maps.google.com/mapfiles/ms/icons/green.png';
 
-
-class MarkerInfoWindow extends React.Component {
-
-    render(){
         return (
             <div>
               <Marker
-                icon={this.props.locationToShow === this.props.marker.title && this.props.isOpen ? { url: iconOpen } : { url: iconDefault }}
-                key={this.props.marker.title}
-                position={{lat: this.props.marker.position.lat, lng: this.props.marker.position.lng}}
-                onClick={() => this.props.onToggleOpen(this.props.marker.title, this.props.marker)}
+                icon={props.placeToShow === props.marker.title && props.isOpen ? { url: iconOpen } : { url: iconDefault }}
+                key={props.marker.title}
+                position={{lat: props.marker.position.lat, lng: props.marker.position.lng}}
+                onClick={() => props.onToggleOpen(props.marker.title, props.marker)}
               >
-                {this.props.locationToShow === this.props.marker.title && this.props.isOpen &&
-                  <InfoWindow onCloseClick={this.props.onToggleOpen}>
-                    <div>
-                      <p className='source'>Read more on: <a target="_blank" href={`https://en.wikipedia.org/wiki/${this.props.marker.title}`}>Wikipedia.org</a></p>
-                      <h1>{this.props.marker.name}</h1>
-                      <p>{this.props.infoContent}</p>
+                {props.placeToShow === props.marker.title && props.isOpen &&
+                  <InfoWindow onCloseClick={props.onToggleOpen}>
+                    <div className='info-window'>
+                      <p className='source'>Read more on: <a target="_blank" href={`https://en.wikipedia.org/wiki/${props.marker.title}`}>Wikipedia.org</a></p>
+                      <h1>{props.marker.name}</h1>
+                      <p>{props.infoContent}</p>
                     </div>
                   </InfoWindow>}
               </Marker>
 
             </div>
         )
-    }
 }
 
 export default MarkerInfoWindow
